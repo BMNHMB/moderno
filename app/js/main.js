bf72d51d3ce3db6be80130ec3d3adeabaefb4d27
BMNHMB <7700108@mail.ru>
2020-06-08 14:21:34 +0300

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,18 @@
 $(function(){
     
-    
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true,
+      });
+      $('.product-slider__inner').slick({
+        dots: true,
+        arrow: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+      });
     var mixer = mixitup('.products__inner-box');
+    
 
 
 });
